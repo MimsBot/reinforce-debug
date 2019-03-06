37f8583aab3be6ffe8c092d90c53be74cff10706
http://www.github.com/mimsbot <mimseyfield@gmail.com>
2019-03-06 11:55:16 -0500

    first commit, syntax error line 18

---
+++ debug.py
@@ -0,0 +1,34 @@
+def select_cards(possible_cards, hand):
+
+    i = 0
+
+    for i, current_card in enumerate(possible_cards):
+        print(f"Do you want to pick up {current_card}?")
+    answer = input()
+    if answer.lower() == 'y':
+        hand.append(current_card)
+        display_hand = (hand)
+    i += 1
+
+    print(f"Your current hand is: {display_hand}".)
+
+    # if len(hand) < 3:
+    #     print("You have too few cards in your hand. Please choose only 3.")
+    #     return False
+    # elif len(hand) > 3:
+    #     print("You have too many cards in your hand. Please choose only 3.")
+    #     return False
+    # else:
+    #     return True
+    # return hand
+
+    # this doesn't work ^
+
+
+available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
+
+new_hand = select_cards(available_cards, [])
+
+display_hand = "\n".join(new_hand)
+
+print("Your new hand is: \n{}".format(display_hand))
